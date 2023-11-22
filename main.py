4e4bcadb9d6011734d2699f517561c040765de20
Justin <justin.pelpoir@efrei.net>
2023-11-22 21:26:06 +0100

    Ajout Menu : - Ajout d'un menu permettant à l'utilisateur d'accéder aux différentes fonctionnalités

--- main.py
+++ main.py
@@ -6,10 +6,42 @@ conversion_minuscule(fichiers)
 
 retrait_ponctuation(fichiers)
 
-#print(fichiers)
-#print(mots_plus_utiliser("cleaned/"))
-#stat_nation("cleaned/")
-
-
 
 # Menu -------------
+a = 0  # Etat "On"
+while a == 0:
+    print("===== ChatBot : v0.1 =====\n"
+          "Ce Chatbot est en cours de développement, des bugs sont susceptibles de se produire.\n\n"
+          "----------\n"      
+          "Menu :  Pour accéder à une fonctionnalité, entrez le numéro qui lui est associé\n\n"
+          "1: Afficher la liste des mots les moins importants dans le corpus\n"
+          "2: Afficher le(s) mot(s) les plus importants\n"
+          "3: Afficher le(s) mot(s) le(s) plus répété(s) par un président\n"
+          "4: Indiquer le(s) nom(s) du (des) président(s) qui a "
+          "(ont) utilisé un mot et celui qui l’a répété le plus de fois\n"
+          "5: Indiquer le premier président à parler de quelque chose\n"
+          "6: Indiquer quel(s) est(sont) le(s) mot(s) que tous les présidents ont évoqués\n\n"
+          "0: Fermer le Chatbot\n\n"
+          )
+
+    choix = int(input("Votre choix : "))
+
+    if choix == 1:
+        print(mots_non_important("cleaned/"))
+    elif choix == 2:
+        print(mots_importants("cleaned/"))
+    elif choix == 3:
+        print(mots_plus_utiliser("cleaned/"))
+    elif choix in [4, 5, 6]:
+        print("En cours de développement, fonctionnalité indisponible.")
+    elif choix == 0:
+        x = int(input("Êtes-vous sûr de vouloir fermer le Chatbot ? 1: Oui ; 2: Non"))
+        if x == 1:
+            a = 1       # Etat "Off"
+
+    if choix != 0:
+        choix = int(input("Retour au menu ? 1: Oui ; 2: Non\n"))
+        if choix == 2:
+            x = int(input("Êtes-vous sûr de vouloir fermer le Chatbot ? 1: Oui ; 2: Non\n"))
+            if x == 1:
+                a = 1           # Etat "Off"
